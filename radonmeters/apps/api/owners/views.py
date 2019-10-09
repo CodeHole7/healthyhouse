afa0f3acdf1fdbbe1b2359a261a79b75dbdbb9c8
galaxydeve <devphs513@gmail.com>
2019-10-09 14:41:35 -0500

    Update views.py

--- radonmeters/apps/api/owners/views.py
+++ radonmeters/apps/api/owners/views.py
@@ -3,9 +3,13 @@ from rest_framework import filters
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAdminUser
+from django.http import JsonResponse
 
 from api.owners.serializers import OwnerSerializer
 from owners.models import Owner
+
+from rest_framework.decorators import list_route
+from rest_framework.permissions import IsAuthenticated
 
 Dosimeter = get_model('catalogue', 'Dosimeter')
 
@@ -22,3 +26,8 @@ class OwnerViewSet(
     filter_backends = (filters.DjangoFilterBackend, filters.SearchFilter)
     filter_fields = ('id', 'email')
     search_fields = ('first_name', 'last_name', 'email')
+
+    @list_route(methods=['GET'], permission_classes=[IsAuthenticated])
+    def get_all(self, request):
+        jsonData = OwnerSerializer(Owner.objects.all(), many=True).data        
+        return JsonResponse({'result':jsonData})
